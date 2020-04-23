55b88a8ca7f11a281ec072563be987e3e143d4bb
Travis Callahan <callahantravis29@gmail.com>
2020-04-23 14:21:23 -0500

    Set up GraphQL Server and Prisma interface

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return item;
+  },
+  //   createDog(parent, args, ctx, info) {
+  //     global.dogs = global.dogs || [];
+  //     // create a dog!
+  //     const newDog = { name: args.name };
+  //     global.dogs.push(newDog);
+  //     return newDog;
+  //   },
+};
 
-module.exports = mutations;
+module.exports = Mutations;
