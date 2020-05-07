dd36bdea9620346dafccf220be6edbb9e2cf453c
Travis Callahan <callahantravis29@gmail.com>
2020-05-07 11:02:39 -0500

    Added pagination and setup user registration on back-end

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -35,6 +38,33 @@ const Mutations = {
     const where = { id: args.id };
     const item = await ctx.db.query.item({ where }, `{ id title}`);
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+
+  async signup(parent, args, ctx, info) {
+    // lowercase user's email
+    args.email = args.email.toLowerCase();
+    // hash + salt password
+    const password = await bcrypt.hash(args.password, 10);
+    // create user in db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] },
+        },
+      },
+      info
+    );
+    // create JWT token for user
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set the JWT as a cookie on the response
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 356, // 1 year cookie
+    });
+    // return user to browser
+    return user;
   },
 };
 
